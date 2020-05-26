8074d5072596e8fffc7b890f468dc0656d10bef5
Suraj Kumar gupta <48393148+surajgupta1288@users.noreply.github.com>
2020-05-26 11:04:58 +0530

    Server Part
    It was developed in python

---
+++ server.py
@@ -0,0 +1,33 @@
+#Server Part:-python
+#To Run Use Your ip Address
+#Developed By:-Suraj Kumar Gupta
+
+from socket import *
+import os
+import platform
+
+host="192.168.42.99"
+port = 9999
+
+s = socket(AF_INET,SOCK_STREAM)
+s.bind((host,port))
+s.listen(5)
+while True:
+	c,addr =  s.accept()
+	type = c.recv(1024).decode("utf-8")
+	if(type == "shutdown"):
+		if(platform.system() == "Windows"):
+			os.system("shutdown /f")
+		elif(platform.system() == "Linux"):
+			os.system("shutdown")
+	elif(type == "restart"):
+		if(platform.system() == "Windows"):
+			os.system("shutdown /r")
+		elif(platform.system() == "Linux"):
+			os.system("shutdown -r")
+	elif(type == "music"):
+		if(platform.system() == "Windows"):
+			os.system("E:\\1.mp3")
+		elif(platform.system() == "Linux"):
+			os.system("~/1.mp3")
+
